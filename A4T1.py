a917cdb4525b36425214484bee386eb88e16af52
cheetiantey <37930535+cheetiantey@users.noreply.github.com>
2021-11-29 23:53:16 -0700

    Add Tracks functionality

--- A4T1.py
+++ A4T1.py
@@ -5,12 +5,11 @@ client = MongoClient()
 
 db = client["A4DBNorm"]
 
+# Deals with Artists
 artists_collection = db["Artists"]
-tracks_collection = db["Tracks"]
 
 with open("Artists.json", "r") as read_file:
     artists_data = json.load(read_file)
-
 
 artists_id = []
 counter = 0
@@ -25,3 +24,20 @@ for document in artists_data:
     counter += 1
 
 ret = artists_collection.insert_many(artists_data)
+
+# Deals with Tracks 
+tracks_collection = db["Tracks"]
+
+with open("Tracks.json", "r") as read_file:
+    tracks_data = json.load(read_file)
+
+tracks_id = []
+counter = 0
+
+for document in tracks_data:
+    tracks_id.append(document["_id"]["$oid"])
+    del document["_id"]
+    document["_id"] = tracks_id[counter]
+    counter += 1
+
+ret = tracks_collection.insert_many(tracks_data)
