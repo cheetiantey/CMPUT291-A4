7c938fbea52f1f1e77f727f61dd512043f827936
cheetiantey <37930535+cheetiantey@users.noreply.github.com>
2021-11-29 23:39:20 -0700

    Change attribute name

--- A4T1.py
+++ A4T1.py
@@ -12,16 +12,16 @@ with open("Artists.json", "r") as read_file:
     artists_data = json.load(read_file)
 
 
-artists_pk = []
+artists_id = []
 counter = 0
 
 # Extract the $oid to be used as the new "_id"
 # Delete the "_id" in each document
 # Append a new extracted "_id" to each document
 for document in artists_data:
-    artists_pk.append(document["_id"]["$oid"])
+    artists_id.append(document["_id"]["$oid"])
     del document["_id"]
-    document["_id"] = artists_pk[counter]
+    document["_id"] = artists_id[counter]
     counter += 1
 
 ret = artists_collection.insert_many(artists_data)
