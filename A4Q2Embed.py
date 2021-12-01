de976fcd8dda86ecb7ccad3050023d5916f9eb63
TheLastWonderor <71833799+TheLastWonderor@users.noreply.github.com>
2021-12-01 00:23:49 -0700

    A4Q2Embed

---
+++ A4Q2Embed.py
@@ -0,0 +1,30 @@
+from pymongo import MongoClient
+
+client = MongoClient()
+
+db = client["A4dbEmbed"]
+
+result = db.ArtistsTracks.aggregate([
+    {
+      "$unwind" : "$tracks"
+    },
+    {"$match": {
+        "tracks.track_id": { "$regex": '^70', "$options": 'i' }
+        }
+    },
+    {
+        "$group":{
+            "_id": "null",
+            "avg_danceability": {"$avg": "$tracks.danceability"}
+        }
+    },
+    {
+    "$project":{
+        "_id": 0,
+        "avg_danceability": 1
+    }
+    }
+])
+
+for i in result:
+    print(i)
