161033b4d4aef5fd9537583060527208bdd4586e
Chee Tey <37930535+cheetiantey@users.noreply.github.com>
2021-12-01 21:06:55 -0700

    Update A4T1.py

--- A4T1.py
+++ A4T1.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-import json
+from bson.json_util import loads
 
 client = MongoClient()
 
@@ -9,35 +9,14 @@ db = client["A4dbNorm"]
 artists_collection = db["Artists"]
 
 with open("artists.json", "r") as read_file:
-    artists_data = json.load(read_file)
+    artists_data = loads(read_file.read())
 
-artists_id = []
-counter = 0
+artists_collection.insert_many(artists_data)
 
-# Extract the $oid to be used as the new "_id"
-# Delete the "_id" in each document
-# Append a new extracted "_id" to each document
-for document in artists_data:
-    artists_id.append(document["_id"]["$oid"])
-    del document["_id"]
-    document["_id"] = artists_id[counter]
-    counter += 1
-
-ret = artists_collection.insert_many(artists_data)
-
-# Deals with Tracks 
+# Deals with Tracks
 tracks_collection = db["Tracks"]
 
 with open("tracks.json", "r") as read_file:
-    tracks_data = json.load(read_file)
+    tracks_data = loads(read_file.read())
 
-tracks_id = []
-counter = 0
-
-for document in tracks_data:
-    tracks_id.append(document["_id"]["$oid"])
-    del document["_id"]
-    document["_id"] = tracks_id[counter]
-    counter += 1
-
-ret = tracks_collection.insert_many(tracks_data)
+tracks_collection.insert_many(tracks_data)
