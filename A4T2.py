1812e7c68ca3acdfdc464465db59cd584a5dd71f
Jordon Ogonoski <jordonogonoski@gmail.com>
2021-12-02 12:31:41 -0500

    changed json use to bson in Task 2

--- A4T2.py
+++ A4T2.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-import json
+from bson.json_util import loads
 
 client = MongoClient()
 
@@ -9,38 +9,19 @@ artists_tracks_collection = db["ArtistsTracks"]
 
 #Artists
 with open("artists.json", "r") as read_file:
-    artists_data = json.load(read_file)
-
-artists_id = []
-counter = 0
-
-# Extract the $oid to be used as the new "_id"
-# Delete the "_id" in each document
-# Append a new extracted "_id" to each document
-for document in artists_data:
-    artists_id.append(document["_id"]["$oid"])
-    del document["_id"]
-    document["_id"] = artists_id[counter]
-    counter += 1
-
+    artists_data = loads(read_file.read())
 
 
 #Tracks
 with open("tracks.json", "r") as read_file:
-    tracks_data = json.load(read_file)
+    tracks_data = loads(read_file.read())
 
-tracks_id = []
-counter = 0
 tracks_dict = {}
 
 for document in tracks_data:
-    tracks_id.append(document["_id"]["$oid"])
-    del document["_id"]
-    document["_id"] = tracks_id[counter]
-    counter += 1
-
     current_track_id = document["track_id"]
     tracks_dict[current_track_id] = document
+
 
 
 for artist in artists_data:
