c3470e08ea42cd9ac9de23072ba940956ec0c64a
Yilin0412 <Yilin25@ulberta.ca>
2021-12-03 00:25:49 -0700

    A4Q4Norm

---
+++ A4Q4Norm.py
@@ -0,0 +1,35 @@
+import datetime
+
+from pymongo import MongoClient
+
+client = MongoClient()
+
+db = client["A4dbNorm"]
+
+result = db.Tracks.aggregate([
+		{"$match": {
+			"release_date": {"$gt": datetime.datetime(1950, 1, 1, 0, 0)}
+			}
+		},
+		{"$lookup":
+			{
+			"from": "Artists",
+			"localField": "artist_ids",
+			"foreignField": "artist_id",
+			"as": "track_info"
+				}
+		},
+		{"$unwind": "$track_info"},
+			"name": "$track_info.name",
+			"t_name": "$name",
+			"t_release_date": "$release_date"
+			}
+		},
+		{"$sort": {"t_release_date": 1}}
+])
+
+for i in result:
+	print(i)
+
+
+
